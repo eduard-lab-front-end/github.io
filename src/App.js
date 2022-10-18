e442ccedff73e36db01d0946148800817b173254
IhorKurylov <idSNrY7I3xhB>
2022-10-18 09:40:48 +0300

    init

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-<>
-<h1>Main</h1>
-  <div>
-    <button>Click</button>
-  </div>
-</>
-  );
+    return (
+        <>
+            <h1>Main</h1>
+            <div>
+                <button>Click</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
