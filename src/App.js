c3f00d8e7c153a74e0e55f4ad7ee8fe68b37cf27
IhorKurylov <idSNrY7I3xhB>
2022-10-18 09:43:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const  App = () => {
+  const handleClick = () => console.log("clicked")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<h2>it's me</h2>
+
+  <button onClick={handleClick}>
+    Click me
+  </button>
+</>
   );
 }
 
